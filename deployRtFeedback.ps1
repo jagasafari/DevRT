cd5bf3cd9f9698b0d26d1775ada5eba78dc742f3
mika <kaszuba.mika@gmail.com>
2017-02-15 23:30:48 +0100

    more complete running script

--- deployRtFeedback.ps1
+++ deployRtFeedback.ps1
@@ -1,7 +1,12 @@
-$runningProcess = Get-Process -name DevRT.Console -ErrorAction SilentlyContinue
-if($runningProcess) { Stop-Process -name DevRT.Console }
-Remove-Item c:\run\RtFeedback.Console\* -Recurse
-Copy-Item c:\DevRT\DevRT.Console\bin\Debug\* c:\run\RtFeedback.Console -Recurse
-cd c:\run\RtFeedback.Console
-.\DevRT.Console.exe
+$processName = 'DevRT.Console'
+$deployPath = 'c:\run\console'
+$packagePath = 'c:\DevRT\DevRT.Console\bin\Debug'
+
+$runningProcess = Get-Process -name $processName -ErrorAction SilentlyContinue
+if($runningProcess) { Stop-Process -name $processName }
+MSBuild.exe /t:clean,build
+if(Test-Path $deployPath) { Remove-Item $deployPath -Recurse }
+Copy-Item $packagePath $deployPath -Recurse
+Write-Host "running dev real time" -foregroundcolor Green
+& (Join-Path $deployPath "$processName.exe")
 
